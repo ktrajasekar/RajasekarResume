c5c9e066102dd0531ed348adecd4194394afb1b2
Rajasekar T <kt.rajasekar@gmail.com>
2021-05-11 20:11:35 +0530

    Minor

--- pages/index.js
+++ pages/index.js
@@ -130,7 +130,7 @@ export default function Home() {
           </div>
           <div className="work-exp-sec">
             <h2>Tenovia E-Commerce [Jan 2014 - Nov 2014] </h2>
-            <h3>Consultant</h3>
+            <h3>Software Engineer</h3>
             <h4>Roles and Responsibility</h4>
             <ul className="ordered-list">
               <li>
