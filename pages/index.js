f3b063d8143d871aa6e296e8d7567b47020e3a5a
Rajasekar T <kt.rajasekar@gmail.com>
2021-05-11 20:01:36 +0530

    Minor update

--- pages/index.js
+++ pages/index.js
@@ -25,13 +25,16 @@ export default function Home() {
               <h1>RAJASEKAR THANGAVEL</h1>
               <p className="tag-line">Front End Developer</p>
               <p className="resume-description">
-                Front End Developer &amp; Hybrid Mobile Application developer with
-                8+ years of professional experience, with innovation, design,
-                coding standards on the web application and cross platform
-                mobile application. Expertise in shopify ecommerce theme
-                development.</p> <p><strong>Specialties</strong> : HTML5, CSS3, VueJS, ReactJS, NodeJS,
-                Angular 2 , EmberJS, Phonegap(Cordova), jQuery, jQuery Mobile,
-                Bootstrap Responsive Framework, Single page application
+                Front End Developer &amp; Hybrid Mobile Application developer
+                with 8+ years of professional experience, with innovation,
+                design, coding standards on the web application and cross
+                platform mobile application. Expertise in shopify ecommerce
+                theme development.
+              </p>{" "}
+              <p>
+                <strong>Specialties</strong> : HTML5, CSS3, VueJS, ReactJS,
+                NodeJS, Angular 2 , EmberJS, Phonegap(Cordova), jQuery, jQuery
+                Mobile, Bootstrap Responsive Framework, Single page application
                 development, LESS, SASS, Javascript, Responsive design,
                 Wordpress Template design, Woocommerce Customization from PSD,
                 Shopify Customization, Semantic code structure, Adobe Photoshop
@@ -68,7 +71,7 @@ export default function Home() {
               <div className="edu-section">
                 <h3>2007 - 2010</h3>
                 <p className="college">
-                  Vel Tech MultiTech - Anna University Afficated{" "}
+                  Vel Tech MultiTech - Anna University Affiliated
                 </p>
                 <p>Bachelor of Computer Science Engineering</p>
               </div>
@@ -85,10 +88,13 @@ export default function Home() {
             <h3>Technical Lead</h3>
             <h4>Roles and Responsibility</h4>
             <ul className="ordered-list">
-            <li>Developing cabin management system</li>
-            <li>Scratch development using Vue.js(VUEJS), Hybrid mobile development, ES6, Websocket communication</li>
-            <li>Service layer using NodeJS and MongoDB</li>
-            <li>SAFe methodology for daily activities</li>
+              <li>Developing cabin management system</li>
+              <li>
+                Scratch development using Vue.js(VUEJS), Hybrid mobile
+                development, ES6, Websocket communication
+              </li>
+              <li>Service layer using NodeJS and MongoDB</li>
+              <li>SAFe methodology for daily activities</li>
             </ul>
           </div>
           <div className="work-exp-sec">
@@ -96,43 +102,67 @@ export default function Home() {
             <h3>Consultant</h3>
             <h4>Roles and Responsibility</h4>
             <ul className="ordered-list">
-              <li>Worked in UI Development and Hybrid Mobile app development using Cordova</li>
-              <li>Using ReactJS as front end layer and NodeJS as a service layer</li>
-              <li>Key Accounts : PayPal, Standard Chartered Bank SG, British Telecom</li>
+              <li>
+                Worked in UI Development and Hybrid Mobile app development using
+                Cordova
+              </li>
+              <li>
+                Using ReactJS as front end layer and NodeJS as a service layer
+              </li>
+              <li>
+                Key Accounts : PayPal, Standard Chartered Bank SG, British
+                Telecom
+              </li>
             </ul>
           </div>
           <div className="work-exp-sec">
-            <h2>Capgemini Consulting  [Dec 2014 - Mar 2016] </h2>
+            <h2>Capgemini Consulting [Dec 2014 - Mar 2016] </h2>
             <h3>Consultant</h3>
             <h4>Roles and Responsibility</h4>
             <ul className="ordered-list">
-              <li>Developing enterprise mobile application using HTML5, CSS3, Javascript, Apache Cordova, jQuery Mobile, Bootstrap and Angularjs to supports major mobile platforms iPhone and Android.</li>
+              <li>
+                Developing enterprise mobile application using HTML5, CSS3,
+                Javascript, Apache Cordova, jQuery Mobile, Bootstrap and
+                Angularjs to supports major mobile platforms iPhone and Android.
+              </li>
               <li>Key Accounts : Aegon Life Insurance, P&amp;G Research</li>
             </ul>
           </div>
           <div className="work-exp-sec">
-            <h2>Tenovia E-Commerce  [Jan 2014 - Nov 2014] </h2>
+            <h2>Tenovia E-Commerce [Jan 2014 - Nov 2014] </h2>
             <h3>Consultant</h3>
             <h4>Roles and Responsibility</h4>
             <ul className="ordered-list">
-              <li>Integrating visual design into major eCommerce platform like Magento, Bigcommerce, Shopify and Woocommerce</li>
+              <li>
+                Integrating visual design into major eCommerce platform like
+                Magento, Bigcommerce, Shopify and Woocommerce
+              </li>
               <li> Functionality development using jQuery and Javascript</li>
-              <li>Convert visuals to HTML layer using Bootstrap framework followed by semantically code structure and SEO guidelines</li>
-              <li>Responsive web design followed to support mobile, tablet and desktops</li>
+              <li>
+                Convert visuals to HTML layer using Bootstrap framework followed
+                by semantically code structure and SEO guidelines
+              </li>
+              <li>
+                Responsive web design followed to support mobile, tablet and
+                desktops
+              </li>
             </ul>
           </div>
           <div class="bottom-bordered"></div>
         </section>
-        
       </main>
 
       <footer>
         <div class="row">
-          <div className="col"><p>Rajasekar Thangavel &copy; 2021</p></div>
-          <div className="col credits-logo text-right">
-            <a href="https://nextjs.org/" target="_blank"><img src="/nextjs-logo.svg" title="credits" /></a></div>
+          <div className="col">
+            <p>Rajasekar Thangavel &copy; 2021</p>
           </div>
-        
+          <div className="col credits-logo text-right">
+            <a href="https://nextjs.org/" target="_blank">
+              <img src="/nextjs-logo.svg" title="credits" />
+            </a>
+          </div>
+        </div>
       </footer>
     </div>
   );
